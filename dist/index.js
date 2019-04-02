2a42982344da26d2d6bd4dd459142899d57dcd51
Benjamim Sonntag <benjamimsonntag@gmail.com>
2019-04-02 23:06:58 +0100

    v0.0.3

--- dist/index.js
+++ dist/index.js
@@ -1,91 +1,111 @@
-'use strict';
+"use strict";
 
 Object.defineProperty(exports, "__esModule", {
   value: true
 });
+exports["default"] = void 0;
 
-var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
+var _react = require("react");
 
-var _react = require('react');
+var _propTypes = _interopRequireDefault(require("prop-types"));
 
-var _propTypes = require('prop-types');
+var _stopMediaStream = _interopRequireDefault(require("stop-media-stream"));
 
-var _propTypes2 = _interopRequireDefault(_propTypes);
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
 
-var _stopMediaStream = require('stop-media-stream');
-
-var _stopMediaStream2 = _interopRequireDefault(_stopMediaStream);
-
-function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+function _typeof(obj) { if (typeof Symbol === "function" && typeof Symbol.iterator === "symbol") { _typeof = function _typeof(obj) { return typeof obj; }; } else { _typeof = function _typeof(obj) { return obj && typeof Symbol === "function" && obj.constructor === Symbol && obj !== Symbol.prototype ? "symbol" : typeof obj; }; } return _typeof(obj); }
 
 function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
 
-function _possibleConstructorReturn(self, call) { if (!self) { throw new ReferenceError("this hasn't been initialised - super() hasn't been called"); } return call && (typeof call === "object" || typeof call === "function") ? call : self; }
+function _defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } }
 
-function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function, not " + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; }
+function _createClass(Constructor, protoProps, staticProps) { if (protoProps) _defineProperties(Constructor.prototype, protoProps); if (staticProps) _defineProperties(Constructor, staticProps); return Constructor; }
 
-var UserMedia = function (_Component) {
+function _possibleConstructorReturn(self, call) { if (call && (_typeof(call) === "object" || typeof call === "function")) { return call; } return _assertThisInitialized(self); }
+
+function _getPrototypeOf(o) { _getPrototypeOf = Object.setPrototypeOf ? Object.getPrototypeOf : function _getPrototypeOf(o) { return o.__proto__ || Object.getPrototypeOf(o); }; return _getPrototypeOf(o); }
+
+function _assertThisInitialized(self) { if (self === void 0) { throw new ReferenceError("this hasn't been initialised - super() hasn't been called"); } return self; }
+
+function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function"); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, writable: true, configurable: true } }); if (superClass) _setPrototypeOf(subClass, superClass); }
+
+function _setPrototypeOf(o, p) { _setPrototypeOf = Object.setPrototypeOf || function _setPrototypeOf(o, p) { o.__proto__ = p; return o; }; return _setPrototypeOf(o, p); }
+
+function _defineProperty(obj, key, value) { if (key in obj) { Object.defineProperty(obj, key, { value: value, enumerable: true, configurable: true, writable: true }); } else { obj[key] = value; } return obj; }
+
+var UserMedia =
+/*#__PURE__*/
+function (_Component) {
   _inherits(UserMedia, _Component);
 
   function UserMedia() {
-    var _ref;
+    var _getPrototypeOf2;
 
-    var _temp, _this, _ret;
+    var _this;
 
     _classCallCheck(this, UserMedia);
 
-    for (var _len = arguments.length, args = Array(_len), _key = 0; _key < _len; _key++) {
+    for (var _len = arguments.length, args = new Array(_len), _key = 0; _key < _len; _key++) {
       args[_key] = arguments[_key];
     }
 
-    return _ret = (_temp = (_this = _possibleConstructorReturn(this, (_ref = UserMedia.__proto__ || Object.getPrototypeOf(UserMedia)).call.apply(_ref, [this].concat(args))), _this), _this.state = {
+    _this = _possibleConstructorReturn(this, (_getPrototypeOf2 = _getPrototypeOf(UserMedia)).call.apply(_getPrototypeOf2, [this].concat(args)));
+
+    _defineProperty(_assertThisInitialized(_this), "state", {
       error: null,
       stream: null
-    }, _temp), _possibleConstructorReturn(_this, _ret);
+    });
+
+    return _this;
   }
 
   _createClass(UserMedia, [{
-    key: 'getUserMedia',
+    key: "getUserMedia",
     value: function getUserMedia() {
       var _this2 = this;
 
       navigator.mediaDevices.getUserMedia(this.props.constraints).then(function (stream) {
-        _this2.setState({ stream: stream });
+        _this2.setState({
+          stream: stream
+        });
+
         _this2.props.onMediaStream(stream);
       }, function (error) {
-        _this2.setState({ error: error });
+        _this2.setState({
+          error: error
+        });
+
         _this2.props.onError(error);
       });
     }
   }, {
-    key: 'componentDidMount',
+    key: "componentDidMount",
     value: function componentDidMount() {
       this.getUserMedia();
     }
   }, {
-    key: 'componentDidUpdate',
+    key: "componentDidUpdate",
     value: function componentDidUpdate(prevProps) {
       if (this.props.constraints !== prevProps.constraints) {
-        (0, _stopMediaStream2.default)(this.state.stream);
+        (0, _stopMediaStream["default"])(this.state.stream);
         this.getUserMedia();
       }
     }
   }, {
-    key: 'componentWillUnmount',
+    key: "componentWillUnmount",
     value: function componentWillUnmount() {
-      (0, _stopMediaStream2.default)(this.state.stream);
+      (0, _stopMediaStream["default"])(this.state.stream);
     }
   }, {
-    key: 'render',
+    key: "render",
     value: function render() {
-      var _props = this.props,
-          children = _props.children,
-          placeholder = _props.placeholder,
-          renderError = _props.renderError;
-      var _state = this.state,
-          error = _state.error,
-          stream = _state.stream;
-
+      var _this$props = this.props,
+          children = _this$props.children,
+          placeholder = _this$props.placeholder,
+          renderError = _this$props.renderError;
+      var _this$state = this.state,
+          error = _this$state.error,
+          stream = _this$state.stream;
 
       if (stream) {
         return children(stream);
@@ -102,15 +122,18 @@ var UserMedia = function (_Component) {
   return UserMedia;
 }(_react.Component);
 
-UserMedia.propTypes = {
-  children: _propTypes2.default.func,
-  constraints: _propTypes2.default.object,
-  onError: _propTypes2.default.func,
-  onMediaStream: _propTypes2.default.func,
-  placeholder: _propTypes2.default.node,
-  renderError: _propTypes2.default.func
-};
-UserMedia.defaultProps = {
+exports["default"] = UserMedia;
+
+_defineProperty(UserMedia, "propTypes", {
+  children: _propTypes["default"].func,
+  constraints: _propTypes["default"].object,
+  onError: _propTypes["default"].func,
+  onMediaStream: _propTypes["default"].func,
+  placeholder: _propTypes["default"].node,
+  renderError: _propTypes["default"].func
+});
+
+_defineProperty(UserMedia, "defaultProps", {
   children: function children() {
     return null;
   },
@@ -125,5 +148,4 @@ UserMedia.defaultProps = {
   renderError: function renderError() {
     return null;
   }
-};
-exports.default = UserMedia;
+});
